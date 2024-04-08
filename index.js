af3a3a46fc22eb9d7caa446fd3d8ea32c84fcf50
DelanoMarcell <delanomartin14@gmail.com>
2024-04-08 14:12:17 +0200

    Refactor code formatting in index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require('express');
+    const express = require('express');
 
-const app = express();
+    const app = express();
 
-app.get('/', (req, res) => {
-    //send html file back
-    res.sendFile(__dirname + '/file.html');
-  
-});
+    app.get('/', (req, res) => {
+        
+    
+        res.send("Welcome to a simple web app, for testing azure web app deployment using github actions")
+    });
 
-const port = process.env.PORT || 3000; // Use environment port or fallback to 3000
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`); 
-});
+    const port = process.env.PORT || 3000; // Use environment port or fallback to 3000
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`); 
+    });
