bffffd60c8979d455cceb7bd70f39e6c18311ae1
DelanoMarcell <delanomartin14@gmail.com>
2024-04-08 19:22:29 +0200

    annoying test again

--- index.js
+++ index.js
@@ -1,90 +1,106 @@
-//create cars api using express
+// //create cars api using express
+// const express = require('express');
+// const app = express();
+
+// //corse
+// const cors = require('cors');
+
+// app.use(cors());
+
+
+// app.use(express.json());
+
+
+// const cars = [
+//     {   
+//         "id": 1,
+//         "make": "Toyota",
+//         "model": "Camry",
+//         "year": 2022,
+//         "price": 250000
+//     },
+//     {
+//         "id": 2,
+//         "make": "Honda",
+//         "model": "Accord",
+//         "year": 2021,
+//         "price": 200000
+//     },
+//     { 
+//         "id": 3,
+//         "make": "Ford",
+//         "model": "Mustang",
+//         "year": 2020,
+//         "price": 300000
+//     }
+
+// ]
+
+// app.get('/', (req, res) => {
+//     res.send('Welcome to the cars API');
+// }
+// );
+
+// app.get('/', (req, res) => {
+//     res.send('This is the home page');
+// });
+
+
+
+
+
+// //get all cars
+// app.get('/cars', (req, res) => {
+//     res.json(cars);
+// });
+
+// //get car by id
+// app.get('/cars/:id', (req, res) => {
+//     const id = req.params.id;
+//     const car = cars.find(car => car.id === id);
+//     res.json(car);
+// });
+
+// //update car
+// app.put('/cars/:id', (req, res) => {
+//     const id = req.params.id;
+//     const updatedCar = req.body;
+//     const index = cars.findIndex(car => car.id === id);
+//     cars[index] = updatedCar;
+//     res.json(updatedCar);
+// });
+
+// //delete car
+// app.delete('/cars/:id', (req, res) => {
+//     const id = req.params.id;
+//     const index = cars.findIndex(car => car.id === id);
+//     cars.splice(index, 1);
+//     res.json({ message: `Car with id ${id} deleted` });
+// });
+
+// //add car
+// app.post('/cars', (req, res) => {
+//     console.log(req);
+//     const newCar = req.body;
+//     console.log(newCar);
+//     cars.push(newCar);
+//     res.json(newCar);
+// });
+
+// //start app at localhost:3000 or prcess.env.PORT
+
+// app.listen(process.env.PORT || 3000, () => {
+//     console.log('Server is running');
+// } );
+
 const express = require('express');
 const app = express();
 
-//corse
-const cors = require('cors');
-
-app.use(cors());
-
-
-app.use(express.json());
-
-
-const cars = [
-    {   
-        "id": 1,
-        "make": "Toyota",
-        "model": "Camry",
-        "year": 2022,
-        "price": 250000
-    },
-    {
-        "id": 2,
-        "make": "Honda",
-        "model": "Accord",
-        "year": 2021,
-        "price": 200000
-    },
-    { 
-        "id": 3,
-        "make": "Ford",
-        "model": "Mustang",
-        "year": 2020,
-        "price": 300000
-    }
-
-]
-
 app.get('/', (req, res) => {
-    res.send('Welcome to the cars API');
-}
-);
-
-
-
-//get all cars
-app.get('/cars', (req, res) => {
-    res.json(cars);
+  res.send('Hello, world!');
 });
 
-//get car by id
-app.get('/cars/:id', (req, res) => {
-    const id = req.params.id;
-    const car = cars.find(car => car.id === id);
-    res.json(car);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
-
-//update car
-app.put('/cars/:id', (req, res) => {
-    const id = req.params.id;
-    const updatedCar = req.body;
-    const index = cars.findIndex(car => car.id === id);
-    cars[index] = updatedCar;
-    res.json(updatedCar);
-});
-
-//delete car
-app.delete('/cars/:id', (req, res) => {
-    const id = req.params.id;
-    const index = cars.findIndex(car => car.id === id);
-    cars.splice(index, 1);
-    res.json({ message: `Car with id ${id} deleted` });
-});
-
-//add car
-app.post('/cars', (req, res) => {
-    console.log(req);
-    const newCar = req.body;
-    console.log(newCar);
-    cars.push(newCar);
-    res.json(newCar);
-});
-
-//start app at localhost:3000 or prcess.env.PORT
-
-app.listen(process.env.PORT || 3000, () => {
-    console.log('Server is running');
-} );
-
-    
