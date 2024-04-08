13c7275776d1e29c449067e19c8f0fa9f8f75eab
DelanoMarcell <delanomartin14@gmail.com>
2024-04-08 14:10:10 +0200

    Remove hostingstart.html and update index.js

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Welcome to a simple web app, for testing azure web app deployment using github actions")
+    //send html file back
+    res.sendFile(__dirname + '/file.html');
+  
 });
 
 const port = process.env.PORT || 3000; // Use environment port or fallback to 3000
