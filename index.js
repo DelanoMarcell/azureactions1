d515e7ff79ed2fc091ca8bbfe064a47282201ecd
DelanoMarcell <delanomartin14@gmail.com>
2024-04-08 13:38:30 +0200

    Update server port configuration

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.get('/', (req, res) => {
     res.send("Welcome to a simple web app, for testing azure web app deployment using github actions")
 });
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+const port = process.env.PORT || 3000; // Use environment port or fallback to 3000
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`); 
 });
